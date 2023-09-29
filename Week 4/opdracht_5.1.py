8cd8a882c1869fc3393eb829c97336e2b2c18313
GitAkash <akashmondalnl@gmail.com>
2023-09-29 22:00:47 +0200

    gitignore

--- Week 4/opdracht_5.1.py
+++ Week 4/opdracht_5.1.py
@@ -11,27 +11,6 @@ mass = np.array(
 
 print(sem(mass))
 
-# b) Als je naar de plot kijkt, begint deze niet bij 0 maar bij 2. Als deze B er niet zou zijn,
-# zou de lijn niet meer door de punten heen lopen maar daar onder.
-#
-# c) in curvefitgui komt de volgende info naar boven: value: 3.3241195246 g/(cm^3) & stderr: 1.1927199018e-02 g/(cm^3)
-# de a is afhankelijke variable oftewel dit gaat over het de density. Aangezien V = 1/D * M + b
-#
-# d) in curvefitgui komt de volgende info naar boven: value: 8.5510732032e-01 g & stderr: 1.3475082074e-01 g voor de b.
-# de b is de onafhankelijke variable oftewel de beker. Het gewicht van de beker blijft immers hetzelfde.
-#
-# e)
-#
-# f) S_min = 4.0228921059e-01
-# from scipy.stats import sem
-# print(sem(mass))
-# geeft 4.8657 g
-# er is dan een verschil van 4.46 gram.
-#
-# g= fitje
-#
-# h)
-
 mylist = []
 for i in mass:
     mylist.append(2)
